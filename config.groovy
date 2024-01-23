8bb50ddf492d6af9d7bb411fddd24f06f5dd5e61
pavankumardasari27 <153920823+pavankumardasari27@users.noreply.github.com>
2024-01-23 10:58:34 +0530

    Delete config.groovy

--- config.groovy
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env groovy
-
-def setEnvFromCredential(credentialId) {
-  withCredentials([string(credentialsId: credentialId, variable: 'ENV')]) {
-    sh "echo '$ENV' > .env" 
-  }
-}
-
-return this
