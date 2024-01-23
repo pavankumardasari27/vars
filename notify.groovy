82373857abdd8049aa12aaab0804c9b635351a61
pavankumardasari27 <153920823+pavankumardasari27@users.noreply.github.com>
2024-01-23 10:59:03 +0530

    Delete notify.groovy

--- notify.groovy
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env groovy
-
-def buildResult(email) {
-  emailext subject: "[${currentBuild.currentResult}] ${env.JOB_NAME}",
-           body: "${currentBuild.currentResult} - ${env.BUILD_URL}",
-           to: email
-}
-
-return this
