ed79ffb613860b70a3f7e6544ad0042272acf681
pavankumardasari27 <153920823+pavankumardasari27@users.noreply.github.com>
2024-01-23 11:22:54 +0530

    utils

---
+++ certbot/Certbot.groovy
@@ -0,0 +1,17 @@
+package com.mycompany.utils
+
+class Certbot {
+
+  def generateCert(email, domain) {
+    sh "certbot certonly --standalone --email ${email} --agree-tos --no-eff-email -d ${domain} --expand"    
+  }
+
+  def renewCerts() {
+    sh "certbot renew"
+  }
+
+  def revokeCert(domain) {
+    sh "certbot revoke --cert-name ${domain} --delete-after-revoke"
+  }
+
+}
---
+++ env/EnvUtils.groovy
@@ -0,0 +1,17 @@
+package com.mycompany.utils
+
+class EnvUtils {
+
+  def loadEnvFile(file) {
+    sh "cp ${file} .env"
+  }
+
+  def getEnvVariable(key) {
+    sh(script: "echo \$${key}", returnStdout: true).trim() 
+  }
+
+  def setEnvVariable(key, value) {
+    sh "export ${key}=${value}"
+  }
+
+}
